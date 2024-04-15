41fa21e7c3c6e75db390a5c62f616a8d301b2924
Vasanthvk99 <143922500+Vasanthvk99@users.noreply.github.com>
2024-04-15 11:35:10 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -3,6 +3,6 @@
 # deployment 
 docker login -u vasanthvk07 -p Vasanthvk@07
 
-docker tag nginx:latest vasanthvk07/prod
+docker tag nginx:latest vasanthvk07/dev
 
-docker push vasanthvk07/prod
+docker push vasanthvk07/dev
